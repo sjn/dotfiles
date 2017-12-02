ffb266a863251109cf65d47640d47a7471586f0d
Salve J. Nilsen <sjn@cpan.org>
2017-12-02 23:14:19 +0100

    Tidy ssh host completion

--- .bashrc.d/ssh-completion.sh
+++ .bashrc.d/ssh-completion.sh
@@ -4,7 +4,7 @@ _ssh()
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts=$(grep '^Host' ~/.ssh/config ~/.ssh/config.d/* | grep -v '[?*]' | cut -d ' ' -f 2-)
+    opts=$(grep '^Host' ~/.ssh/config ~/.ssh/config.d/* 2>&- | grep -v '[?*]' | cut -d ' ' -f 2-)
 
     COMPREPLY=( $(compgen -W "$opts" -- ${cur}) )
     return 0
